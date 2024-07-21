de99e8e1c558bae1e2c8bac4a046cacb499e5775
fless <fless@inbox.ru>
2024-07-21 20:33:26 +0300

    Added new function of three params signature method

--- src/OnLesson/thereIsAMethod.java
+++ src/OnLesson/thereIsAMethod.java
@@ -10,9 +10,39 @@ public class thereIsAMethod {
    static LocalDate dateNow;
    static int addDaysFor;
 
+    public static void main(String[] args) {
+        System.out.println("Текущее время и дата :" + LocalDateTime.now());
+        System.out.println("Текущее время и дата :" + timeNowFor());
+        System.out.println("Сегодня дата:" + LocalDate.now());
+        System.out.println("Текущая дата" + getDay());
+
+
+
+        System.out.println("Введите число дней: ");
+        addDaysFor = new Scanner(System.in).nextInt();
+        System.out.println("Получим дату"+addDay(addDaysFor));
+
+        System.out.println("ВВедите сколько дней надо добавить: ");
+        int addDateFor = new Scanner(System.in).nextInt();
+        System.out.println("Введите сколько часов добавить :");
+        int addHourFor = new Scanner(System.in).nextInt();
+        System.out.println("Введите сколько минут добавить");
+        int addMinutesFor = new Scanner(System.in).nextInt();
+        System.out.println(addDateAndTime(addDateFor,addHourFor,addMinutesFor));
+
+
+        while (true) {
+            addDay();
+        }
+
+
+
+
+    }
+
     public static LocalDateTime timeNowFor() {
         return timenowformethod = LocalDateTime.now();
-  }
+    }
 
     public static LocalDate getDay() {
         return dateNow = LocalDate.now();
@@ -28,26 +58,17 @@ public class thereIsAMethod {
         return  LocalDate.now().plusDays(addDaysFor);
     }
 
+    public static LocalDateTime addDateAndTime (int addDaysFor, int addHourFor, int addMinuteFor) {
+        LocalDateTime timeNow = LocalDateTime.now();
 
+        timeNow =  LocalDateTime.from(timeNow).plusDays(addDaysFor);
 
+        timeNow = LocalDateTime.from(timeNow).plusHours(addHourFor);
 
-    public static void main(String[] args) {
-        System.out.println("Текущее время и дата :" + LocalDateTime.now());
-        System.out.println("Текущее время и дата :" + timeNowFor());
-        System.out.println("Сегодня дата:" + LocalDate.now());
-        System.out.println("Текущая дата" + getDay());
-
-
-
-        System.out.println("Введите число дней: ");
-        addDaysFor = new Scanner(System.in).nextInt();
-        System.out.println("Получим дату"+addDay(addDaysFor));
-
-
-        while (true) {
-            addDay();
-        }
+        timeNow = LocalDateTime.from(timeNow).plusMinutes(addMinuteFor);
+        return timeNow;
 
 
     }
+
 }
