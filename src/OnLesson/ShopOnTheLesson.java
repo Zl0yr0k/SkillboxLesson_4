42a70ca2f11bc183e8451f689952fb58891fc55c
fless <fless@inbox.ru>
2024-07-27 17:16:04 +0300

    I write new function and use nextLine() and nextInt()

---
+++ src/OnLesson/ShopOnTheLesson.java
@@ -0,0 +1,49 @@
+package OnLesson;
+
+import java.util.Scanner;
+
+public class ShopOnTheLesson {
+    private static String items = "";
+
+    public static void main(String[] args) {
+
+
+        add(getName(), getPrice());
+        add(getName(), getPrice());
+        add("Сахар", 90);
+        print("Содержимое корзины: ");
+        clear();
+        print("Содержимое:");
+    }
+
+    public static int getPrice () {
+        System.out.println("Введите цену: ");
+        int price = new Scanner(System.in).nextInt();
+        return price;
+    }
+
+    public static String getName () {
+        System.out.println("Введите наименование продукта: ");
+        Scanner na = new Scanner(System.in);
+        String name = na.nextLine();
+        return name;
+    }
+
+    public static void add (String name, int price ) {
+        items = items + " \n" + name + " - " + price;
+    }
+
+    public static void clear () {
+        items = "";
+    }
+
+    public static void print (String title) {
+        System.out.println(title);
+        if (items.isEmpty()) {
+            System.out.println("Корзина пуста");
+        } else {
+            System.out.println(items);
+        }
+    }
+
+}
