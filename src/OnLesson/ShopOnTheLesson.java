2facc3558e6fd302228d68e83e0fe0eb9783379c
fless <fless@inbox.ru>
2024-08-13 20:30:19 +0300

    I don't resolve problem with exit programme

--- src/OnLesson/ShopOnTheLesson.java
+++ src/OnLesson/ShopOnTheLesson.java
@@ -4,18 +4,31 @@ import java.util.Scanner;
 
 public class ShopOnTheLesson {
     private static String items = "";
+    private static int totalPrice;
+    private static boolean commandOfExit = true;
+
 
     public static void main(String[] args) {
 
+        while (commandOfExit) {
+            add(getName(), getPrice());
+            }
 
-        add(getName(), getPrice());
-        add(getName(), getPrice());
-        add("Сахар", 90);
+
+        System.out.println(getTotalPrice());
+        printTotalPrice();
+        System.out.println();
         print("Содержимое корзины: ");
         clear();
         print("Содержимое:");
         thereIsAMethod time = new thereIsAMethod();
         System.out.println(time.getDay());
+    }
+
+    public static void add (String name, int price ) {
+
+        items = items + " \n" + name + " - " + price;
+        totalPrice = totalPrice + price;
     }
 
     public static int getPrice () {
@@ -28,15 +41,25 @@ public class ShopOnTheLesson {
         System.out.println("Введите наименование продукта: ");
         Scanner na = new Scanner(System.in);
         String name = na.nextLine();
+        if (name.equals("exit") ){
+            commandOfExit = false;
+        }
+
         return name;
     }
 
-    public static void add (String name, int price ) {
-        items = items + " \n" + name + " - " + price;
+
+    public static void printTotalPrice () {
+        System.out.println(totalPrice);
+    }
+
+    public static int getTotalPrice() {
+        return totalPrice;
     }
 
     public static void clear () {
         items = "";
+        totalPrice = 0;
     }
 
     public static void print (String title) {
