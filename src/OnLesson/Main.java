427a78cf10a7007ddc96d5bbd5d0a30c9b6e5aed
fless <fless@inbox.ru>
2024-08-18 16:44:54 +0300

    I do separate

--- src/OnLesson/Main.java
+++ src/OnLesson/Main.java
@@ -1,4 +1,24 @@
 package OnLesson;
 
 public class Main {
-}
+
+
+    public static void main(String[] args) {
+        ShopOnTheLesson UdginBasket = new ShopOnTheLesson();
+        UdginBasket.cycleOfGetNameAndGetPrice();
+        System.out.println(UdginBasket.getTotalPrice());
+        UdginBasket.printTotalPrice();
+        System.out.println();
+        UdginBasket.print("Содержимое корзины: ");
+        UdginBasket.clear();
+        UdginBasket.print("Содержимое:");
+        thereIsAMethod time = new thereIsAMethod();
+        System.out.println(time.getDay());
+
+        ShopOnTheLesson OlgaBasket = new ShopOnTheLesson();
+        OlgaBasket.cycleOfGetNameAndGetPrice();
+    }
+    }
+
+
+
